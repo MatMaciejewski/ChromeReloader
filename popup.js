49e06d01892ac59c7c8962b872771e2768746541
mmaciejewski <mateusz.maciejewski@apollogic.com>
2017-10-01 16:49:01 +0200

    Final

--- popup.js
+++ popup.js
@@ -1,117 +1,39 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback called when the URL of the current tab
- *   is found.
- */
-function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
-
-  chrome.tabs.query(queryInfo, (tabs) => {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    callback(url);
-  });
-}
-
-
-
-async function getCurrentTabIndex(){
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
-  chrome.tabs.query(queryInfo, (tabs) => {
-    console.log(tabs);
-    console.log("INSIDE GET");
-  });
-
-  console.log("Poszukiwany ",url)
-  var queryInfo = {
-      currentWindow: true
-  }
-  chrome.tabs.query(queryInfo, (tabs) => {
-    for (i = 0; i < tabs.length; i++) {
-      if( tabs[i].url == url ){
-        console.log("Index GET ",i);
-        console.log("Index GET ",i);
-        return i
-      }
-      console.log("NOPE ",tabs[i].url)
-      // await sleep(500);
-      // await sleep(500);
-      // chrome.tabs.reload(tabs[i].id);
-    }
-  });
-
-  return -1;
-}
-
-
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function reloader(tab){
-  if(tab.length>0){
-    await sleep(1000);
-    chrome.tabs.reload(tab[0].id);
-    reloader(tab.slice(1,));
-  }
-}
-
-async function selectiveReloader(tab, amount){
+async function reloader(tab,amount){
   if(tab.length>0&&amount>0){
     await sleep(1000);
     chrome.tabs.reload(tab[0].id);
-    selectiveReloader(tab.slice(1,),amount-1);
+    reloader(tab.slice(1,),amount-1);
   }
 }
 
-function reloadAllTabs(){
+function reloadAllTabs(toRight){
   var queryInfo = {
     currentWindow: true
   };
   chrome.tabs.query(queryInfo, (tabs) => {
-    chrome.tabs.query({
-      active: true,
-      currentWindow: true
-    }, (act) => {
-      var index = -1;
-      for(i = 0;i<tabs.length;i++){
-        if(tabs[i].url == act[0].url){
-          index=i;
-          break;
+    if(toRight==true){
+      chrome.tabs.query({
+        active: true,
+        currentWindow: true
+      }, (act) => {
+        var index = -1;
+        for(i = 0;i<tabs.length;i++){
+          if(tabs[i].url == act[0].url){
+            index=i;
+            break;
+          }
         }
-      }
-      if(index<tabs.length-1){
-        reloader(tabs.slice(index+1,));
-      }
-    })
+        if(index<tabs.length-1){
+          reloader(tabs.slice(index+1,),tabs.length);
+        }
+      })
+    }else{
+      reloader(tabs)
+    }
   });
 }
 
@@ -132,22 +54,23 @@ function reloadNumberOfTabs(amount){
         }
       }
       if(index<tabs.length-1){
-        selectiveReloader(tabs.slice(index+1,),amount);
+        reloader(tabs.slice(index+1,),amount);
       }
     })
   });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  getCurrentTabUrl((url) => {
     var tabNum = document.getElementById('tabNum');
     tabNum.addEventListener('change', () => {
       reloadNumberOfTabs(tabNum.value);
     });
-    var button = document.getElementById('reloadAll');
-    button.addEventListener('click', () => {
-      console.log("Button");
+    var buttonAll = document.getElementById('reloadAll');
+    buttonAll.addEventListener('click', () => {
       reloadAllTabs();
-    })
-  });
+    });
+    var buttonRight = document.getElementById('reloadAllTabsToTheRight');
+    buttonRight.addEventListener('click', () => {
+      reloadAllTabs(true);
+    });
 });
